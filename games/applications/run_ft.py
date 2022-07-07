f996aaeb5a202ef68de18e74449ef1edcd4a93bd
Raynel Sanchez <raynelfss@hotmail.com>
2022-07-06 21:35:28 -0400

    Lint-related modifications

--- games/applications/run_ft.py
+++ games/applications/run_ft.py
@@ -1,12 +1,14 @@
-import os
-
-import pytest
-import random
-
-from applications import backends, run_qiskit_circuit
+# ------------------------------------------------------------------------------
+# Part of Red Queen Project.  This file is distributed under the MIT License.
+# See accompanying file /LICENSE for details.
+# ------------------------------------------------------------------------------
 
 """Benchmark for Quantum Fourier Transform"""
 
+import os
+import random
+import pytest
+from applications import backends, run_qiskit_circuit
 import numpy as np
 
 # Importing standard Qiskit libraries
@@ -20,9 +22,9 @@ def generate_ft_circuit_1(integer_value):
     qubits = QuantumRegister(len(binary))
     bits = ClassicalRegister(len(binary))
     qc = QuantumCircuit(qubits, bits, name="main")
-    for i in range(len(binary)):
-        if binary[i] == "1":
-            qc.x((len(qubits)-1) - i)
+    for digit, number in enumerate(binary):
+        if number == "1":
+            qc.x((len(qubits)-1) - digit)
     qc.barrier()
     # Let's try and recreate the quantum Fourier Transform SubCircuit from the paper:
     for qubit in range(len(qubits)-1,-1,-1):
@@ -32,13 +34,11 @@ def generate_ft_circuit_1(integer_value):
                 qc.crz(np.pi/np.power(2,subqubit-(qubit - 1)),subqubit-1, subqubit)
                 # print(qubit, subqubit, subqubit-1, np.power(2,subqubit-(qubit - 1)))
     qc.barrier()
-    
     # Now that the circuit was implemented successfully (??) We can continue implementing our gates:
     for qubit in range(len(qubits)):
         qc.rz(np.pi/np.power(2,qubit), qubit)
         #print(qubit, "pi/" + str(np.power(2,qubit)))
     qc.barrier()
-    
     # Finally we implement the circuit backwards, we also negate the rotations done at the beginning.
     for qubit in range(len(qubits)):
         if not qubit == 0:
@@ -47,7 +47,6 @@ def generate_ft_circuit_1(integer_value):
                 # print(qubit, subqubit, subqubit-1, np.power(2,subqubit-(qubit - 1)))
         qc.h(qubit)
     qc.barrier()
-    
     # We can now measure and return the circuit
     for qubit in range(len(qubits)):
         qc.measure(qubit, qubit)
@@ -83,7 +82,8 @@ def generate_ft_circuit_2(integer_value):
 def bench_qiskit_ft(benchmark, optimization_level, backend, method):
     shots = 65536
     binary = format(SECRET_INT, "b")
-    expected_counts = { format((SECRET_INT+1)%(2**(len(binary))), "b").zfill(len(binary)) : shots} if method == "1" else {format(SECRET_INT, "b") : shots}
+    binary_1 = format((SECRET_INT+1)%(2**(len(binary))), "b").zfill(len(binary))
+    expected_counts = { binary_1 : shots} if method == "1" else {binary : shots}
     if method == "1":
         benchmark.name = "Quantum Fourier Transform v1"
         circ = QuantumCircuit.from_qasm_file(os.path.join(QASM_DIR, f"ft_1_{SECRET_INT}.qasm"))
