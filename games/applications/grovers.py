855d33eb863c8adec9bc47dae5c6ed70b5906eb1
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2022-07-14 07:59:09 -0400

    Grover Benchmark Code Adopted from SRI-International

--- games/applications/grovers.py
+++ games/applications/grovers.py
@@ -1,8 +1,48 @@
+# Copyright 2022 SRI-International organization
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# cff-version: 1.2.0
+# "This code below was adapted from the code created by the SRI-International organization."
+# The names listed below are the orginal authors of the code
+# authors:
+#   - family-names: Lubinski
+#     given-names: Thomas
+#   - family-names: Johri
+#     given-names: Sonika
+#   - family-names: Varosy
+#     given-names: Paul
+#   - family-names: Coleman
+#     given-names: Jeremiah
+#   - family-names: Zhao
+#     given-names: Luning
+#   - family-names: Necaise
+#     given-names: Jason
+#   - family-names: Baldwin
+#     given-names: Charles
+#   - family-names: Mayer
+#     given-names: Karl
+#   - family-names: Proctor
+#     given-names: Timothy
+# title: "Application-Oriented Performance Benchmarks for Quantum Computing"
+# date-released: 2021-10-7
+# url: "https://arxiv.org/abs/2110.03137"
+# github repository: https://github.com/SRI-International/q_c-App-Oriented-Benchmarks/blob/master/grovers/qiskit/grovers_benchmark.py
+
 """
 Grover's Search Benchmark Program - Qiskit
 """
 import os
-import secrets
 
 
 import pytest
@@ -16,178 +56,183 @@ QASM_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qasm//grove
 
 np.random.seed(0)
 
-_use_mcx_shim = False 
-
-# saved circuits for display
-QC_ = None
-grover_oracle = None
-diffusion_operator = None
-
-QubitCount = np.random.randint(2,5)
-SECRET_STATE = np.random.randint(0,(2**QubitCount))
 
 ############### Circuit Definition
 
-def GroversSearch(num_qubits, marked_item):
-    n_iterations = int(np.pi * np.sqrt(2 ** num_qubits) / 4)
+
+def grovers_search(num_qubits, marked_item):
+    n_iterations = int(np.pi * np.sqrt(2**num_qubits) / 4)
     # allocate qubits
-    qr = QuantumRegister(num_qubits);
-    cr = ClassicalRegister(num_qubits);
-    qc = QuantumCircuit(qr, cr, name="main")
+    q_r = QuantumRegister(num_qubits)
+    c_r = ClassicalRegister(num_qubits)
+    q_c = QuantumCircuit(q_r, c_r, name="main")
 
     # Start with Hadamard on all qubits
     for i_qubit in range(num_qubits):
-        qc.h(qr[i_qubit])
+        q_c.h(q_r[i_qubit])
 
     # loop over the estimated number of iterations
     for _ in range(n_iterations):
 
-        # qc.barrier()
-    
+        # q_c.barrier()
         # add the grover oracle
-        qc.append(add_grover_oracle(num_qubits, marked_item).to_instruction(), qr)
-        
-        # add the diffusion operator
-        qc.append(add_diffusion_operator(num_qubits).to_instruction(), qr)
+        q_c.append(add_grover_oracle(num_qubits, marked_item).to_instruction(), q_r)
 
-    # qc.barrier()
-        
+        # add the diffusion operator
+        q_c.append(add_diffusion_operator(num_qubits).to_instruction(), q_r)
+
+    # q_c.barrier()
+
     # measure all qubits
-    qc.measure(qr, cr)
+    q_c.measure(q_r, c_r)
 
     # save smaller circuit example for display
-    global QC_    
-    if QC_ == None or num_qubits <= 5:
-        if num_qubits < 9: QC_ = qc
-        
+    global QC_
+    if QC_ is None or num_qubits <= 5:
+        if num_qubits < 9:
+            QC_ = q_c
+
     # return a handle on the circuit
-    return qc
+    return q_c
+
 
 ############## Grover Oracle
 
+
 def add_grover_oracle(num_qubits, marked_item):
-    global grover_oracle
-    
+    global GROVER_ORACLE
+
     marked_item_bits = format(marked_item, f"0{num_qubits}b")[::-1]
 
-    qr = QuantumRegister(num_qubits); qc = QuantumCircuit(qr, name="oracle")
+    q_r = QuantumRegister(num_qubits)
+    q_c = QuantumCircuit(q_r, name="oracle")
 
-    for (q, bit) in enumerate(marked_item_bits):
+    for (q_var, bit) in enumerate(marked_item_bits):
         if not int(bit):
-            qc.x(q)
+            q_c.x(q_var)
 
-    qc.h(num_qubits - 1)
-    
-    if _use_mcx_shim:
-        add_mcx(qc, [x for x in range(num_qubits - 1)], num_qubits - 1)
+    q_c.h(num_qubits - 1)
+
+    if _USE_MCX_SHIM:
+        add_mcx(q_c, list(range(num_qubits - 1)), num_qubits - 1)
     else:
-        qc.mcx([x for x in range(num_qubits - 1)], num_qubits - 1)
-        
-    qc.h(num_qubits - 1)
+        q_c.mcx(list(range(num_qubits - 1)), num_qubits - 1)
 
-    # qc.barrier()
+    q_c.h(num_qubits - 1)
 
-    for (q, bit) in enumerate(marked_item_bits):
+    # q_c.barrier()
+
+    for (q_var, bit) in enumerate(marked_item_bits):
         if not int(bit):
-            qc.x(q)
+            q_c.x(q_var)
 
-    if grover_oracle == None or num_qubits <= 5:
-        if num_qubits < 9: grover_oracle = qc
-        
-    return qc
+    if GROVER_ORACLE is None or num_qubits <= 5:
+        if num_qubits < 9:
+            GROVER_ORACLE = q_c
+
+    return q_c
+
 
 ############## Grover Diffusion Operator
 
+
 def add_diffusion_operator(num_qubits):
-    global diffusion_operator
+    global DIFFUSION_OPERATOR
 
-    qr = QuantumRegister(num_qubits); qc = QuantumCircuit(qr, name="diffuser")
+    q_r = QuantumRegister(num_qubits)
+    q_c = QuantumCircuit(q_r, name="diffuser")
 
     for i_qubit in range(num_qubits):
-        qc.h(qr[i_qubit])
+        q_c.h(q_r[i_qubit])
     for i_qubit in range(num_qubits):
-        qc.x(qr[i_qubit])
-    qc.h(num_qubits - 1)
-    
-    if _use_mcx_shim:
-        add_mcx(qc, [x for x in range(num_qubits - 1)], num_qubits - 1)
+        q_c.x(q_r[i_qubit])
+    q_c.h(num_qubits - 1)
+
+    if _USE_MCX_SHIM:
+        add_mcx(q_c, list(range(num_qubits - 1)), num_qubits - 1)
     else:
-        qc.mcx([x for x in range(num_qubits - 1)], num_qubits - 1)
-        
-    qc.h(num_qubits - 1)
+        q_c.mcx(list(range(num_qubits - 1)), num_qubits - 1)
 
-    # qc.barrier()
+    q_c.h(num_qubits - 1)
+
+    # q_c.barrier()
 
     for i_qubit in range(num_qubits):
-        qc.x(qr[i_qubit])
+        q_c.x(q_r[i_qubit])
     for i_qubit in range(num_qubits):
-        qc.h(qr[i_qubit])
-        
-    if diffusion_operator == None or num_qubits <= 5:
-        if num_qubits < 9: diffusion_operator = qc
-        
-    return qc
+        q_c.h(q_r[i_qubit])
+
+    if DIFFUSION_OPERATOR is None or num_qubits <= 5:
+        if num_qubits < 9:
+            DIFFUSION_OPERATOR = q_c
+
+    return q_c
+
 
 ############### MCX shim
 
 # single cx / cu1 unit for mcx implementation
-def add_cx_unit(qc, cxcu1_unit, controls, target):
+def add_cx_unit(q_c, cxcu1_unit, controls, target):
     num_controls = len(controls)
     i_qubit = cxcu1_unit[1]
     j_qubit = cxcu1_unit[0]
     theta = cxcu1_unit[2]
-    
-    if j_qubit != None:
-        qc.cx(controls[j_qubit], controls[i_qubit]) 
-    qc.cu1(theta, controls[i_qubit], target)
-        
+
+    if j_qubit is not None:
+        q_c.cx(controls[j_qubit], controls[i_qubit])
+    q_c.cu1(theta, controls[i_qubit], target)
+
     i_qubit = i_qubit - 1
-    if j_qubit == None:
+    if j_qubit is None:
         j_qubit = i_qubit + 1
     else:
         j_qubit = j_qubit - 1
-        
+
     if theta < 0:
         theta = -theta
-    
+
     new_units = []
     if i_qubit >= 0:
-        new_units += [ [ j_qubit, i_qubit, -theta ] ]
-        new_units += [ [ num_controls - 1, i_qubit, theta ] ]
-        
+        new_units += [[j_qubit, i_qubit, -theta]]
+        new_units += [[num_controls - 1, i_qubit, theta]]
+
     return new_units
 
-# mcx recursion loop 
-def add_cxcu1_units(qc, cxcu1_units, controls, target):
+
+# mcx recursion loop
+def add_cxcu1_units(q_c, cxcu1_units, controls, target):
     new_units = []
     for cxcu1_unit in cxcu1_units:
-        new_units += add_cx_unit(qc, cxcu1_unit, controls, target)
+        new_units += add_cx_unit(q_c, cxcu1_unit, controls, target)
     cxcu1_units.clear()
     return new_units
+
 
 # mcx gate implementation: brute force and inefficent
 # start with a single CU1 on last control and target
 # and recursively expand for each additional control
-def add_mcx(qc, controls, target):
+def add_mcx(q_c, controls, target):
     num_controls = len(controls)
     theta = np.pi / 2**num_controls
-    qc.h(target)
-    cxcu1_units = [ [ None, num_controls - 1, theta] ]
+    q_c.h(target)
+    cxcu1_units = [[None, num_controls - 1, theta]]
     while len(cxcu1_units) > 0:
-        cxcu1_units += add_cxcu1_units(qc, cxcu1_units, controls, target)
-    qc.h(target)
+        cxcu1_units += add_cxcu1_units(q_c, cxcu1_units, controls, target)
+    q_c.h(target)
 
 
-# def GroversSearch(num_qubits, marked_item, n_iterations)
+# def grovers_search(num_qubits, marked_item, n_iterations)
 def grovers_dist(num_qubits, marked_item, n_iterations):
     dist = {}
     for i in range(2**num_qubits):
         key = bin(i)[2:].zfill(num_qubits)
-        theta = np.arcsin(1/np.sqrt(2 ** num_qubits))
+        theta = np.arcsin(1 / np.sqrt(2**num_qubits))
         if i == marked_item:
-            dist[key] = np.sin((2*n_iterations+1)*theta)**2
+            dist[key] = np.sin((2 * n_iterations + 1) * theta) ** 2
         else:
-            dist[key] = (np.cos((2*n_iterations+1)*theta)/(np.sqrt(2 ** num_qubits - 1)))**2
+            dist[key] = (
+                np.cos((2 * n_iterations + 1) * theta) / (np.sqrt(2**num_qubits - 1))
+            ) ** 2
     return dist
 
 
@@ -198,15 +243,26 @@ def bench_qiskit_grovers(benchmark, optimization_level, backend):
     shots = 70000
     expected_counts = {format(SECRET_STATE, "b").zfill(QubitCount): shots}
     benchmark.name = "Grover's Search Algorithm"
-    circ = QuantumCircuit.from_qasm_file(os.path.join(QASM_DIR, f"grover{QubitCount}-{SECRET_STATE}.qasm"))
+    circ = QuantumCircuit.from_qasm_file(
+        os.path.join(QASM_DIR, f"grover{QubitCount}-{SECRET_STATE}.qasm")
+    )
     benchmark.algorithm = f"Optimization level: {optimization_level} on {backend.name()}"
     run_qiskit_circuit(benchmark, circ, backend, optimization_level, shots, expected_counts)
 
 
 if __name__ == "__main__":
-    GroversSearch(QubitCount, SECRET_STATE).qasm(filename=os.path.join(QASM_DIR, f"grover{QubitCount}-{SECRET_STATE}.qasm"))
+    # saved circuits for display
+    QC_ = None
+    GROVER_ORACLE = None
+    DIFFUSION_OPERATOR = None
+    QubitCount = np.random.randint(2, 5)
+    SECRET_STATE = np.random.randint(0, (2**QubitCount))
+    grovers_search(QubitCount, SECRET_STATE).qasm(
+        filename=os.path.join(QASM_DIR, f"grover{QubitCount}-{SECRET_STATE}.qasm")
+    )
+    _USE_MCX_SHIM = False
 
 # This for loop was used to create the necessary qasm files for this benchmark
- # for i in range(2,6):
-    #     for special_num in range((2**i)):
-    #         GroversSearch(i, special_num).qasm(filename=os.path.join(QASM_DIR, f"grover{i}-{special_num}.qasm"))
+# for i in range(2,6):
+#     for special_num in range((2**i)):
+#         grovers_search(i, special_num).qasm(filename=os.path.join(QASM_DIR, f"grover{i}-{special_num}.qasm"))
