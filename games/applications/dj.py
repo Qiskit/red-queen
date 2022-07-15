1d7acae7e11b8c9d3d9ac397223bb642ba6424be
Danielle Odigie <odigiedanielle@gmail.com>
2022-07-15 10:12:47 -0400

    fixing lint issues again

--- games/applications/dj.py
+++ games/applications/dj.py
@@ -9,7 +9,7 @@ from . import backends, run_qiskit_circuit
 
 
 QASM_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qasm")
-num = 5
+NUM = 5
 # -----------------------------------------------------------------------------------
 
 
@@ -19,9 +19,9 @@ def constant(n):
     output = np.random.randint(2)
     if output == 1:
         qc.x(n)
-    oracle = qc.to_gate()
-    oracle.name = "Slayyy Oracle"
-    return oracle
+    c_oracle = qc.to_gate()
+    c_oracle.name = "Slayyy Oracle"
+    return c_oracle
 
 
 def balanced(n):
@@ -29,17 +29,17 @@ def balanced(n):
     qc = QuantumCircuit(n + 1)
     b = np.random.randint(1, 2**n)
     bstr = format(b, "0" + str(n) + "b")
-    for qubit in range(len(bstr)):
+    for qubit in enumerate(bstr):
         if bstr[qubit] == "1":
             qc.x(qubit)
     for qubit in range(n):
         qc.cx(qubit, n)
-    for qubit in range(len(bstr)):
+    for qubit in enumerate(bstr):
         if bstr[qubit] == "1":
             qc.x(qubit)
-    oracle = qc.to_gate()
-    oracle.name = "Slayyy Oracle"
-    return oracle
+    b_oracle = qc.to_gate()
+    b_oracle.name = "Slayyy Oracle"
+    return b_oracle
 
 
 def oracle(case, n):
@@ -48,16 +48,17 @@ def oracle(case, n):
         return balanced(n)
     if case == "constant":
         return constant(n)
+    return None
 
 
-def dj_alg(oracle, n):
+def dj_alg(ora_cle, n):
     """Creates Deustch Jozsa circuit"""
     dj = QuantumCircuit(n + 1, n)
     dj.x(n)
     dj.h(n)
     for i in range(n):
         dj.h(i)
-    dj.append(oracle, range(n + 1))
+    dj.append(ora_cle, range(n + 1))
     for i in range(n):
         dj.h(i)
     for i in range(n):
@@ -81,16 +82,16 @@ def bench_qiskit_dj(benchmark, optimization_level, backend, case):
     shots = 20000
     if case == "balanced":
         benchmark.name = "Deutsch Jozsa - Balanced Oracle"
-        expected_counts = {str("1" * num): shots}
+        expected_counts = {str("1" * NUM): shots}
         circ = QuantumCircuit.from_qasm_file(os.path.join(QASM_DIR, "dj_balanced.qasm"))
     else:
         benchmark.name = "Deutsch Jozsa - Constant Oracle"
-        expected_counts = {str("0" * num): shots}
+        expected_counts = {str("0" * NUM): shots}
         circ = QuantumCircuit.from_qasm_file(os.path.join(QASM_DIR, "dj_constant.qasm"))
     benchmark.algorithm = f"Optimization level: {optimization_level} on {backend.name()}"
     run_qiskit_circuit(benchmark, circ, backend, optimization_level, shots, expected_counts)
 
 
 if __name__ == "__main__":
-    build_dj("balanced", num).qasm(filename=os.path.join(QASM_DIR, "dj_balanced.qasm"))
-    build_dj("constant", num).qasm(filename=os.path.join(QASM_DIR, "dj_constant.qasm"))
+    build_dj("balanced", NUM).qasm(filename=os.path.join(QASM_DIR, "dj_balanced.qasm"))
+    build_dj("constant", NUM).qasm(filename=os.path.join(QASM_DIR, "dj_constant.qasm"))
