390ee1cb9adf84f358dad572c5c2f3bacd0db7d4
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2022-07-18 18:09:30 -0400

    Updated Benchmark Retrieval Code

--- red-queen.py
+++ red-queen.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import os
+from os import walk
 from contextlib import redirect_stderr
 import subprocess as sub
 from zoneinfo import available_timezones
@@ -8,60 +9,46 @@ from zoneinfo import available_timezones
 
 def benchmarkRetrieval():
     benchmark_category = {}
-    benchmarks_types = []
-    benchmarks = []
-    a = os.listdir('games')
-    for word in a:
-        if word[0] == ".":
-            pass
-        else:
-            benchmark_category[word] = []
-    for k in benchmark_category.keys():
-        benchmarks = os.listdir(f'games/{k}')
-        for word in benchmarks:
-            if word[0] == ".":
-                pass
-            elif word[0] == "_":
-                pass
-            elif word[::-1][:2][::-1] != "py":
-                pass
-            else:
-                benchmark_category[k].append(word)
-    for k in benchmark_category.keys():
-        benchmarks_types.append(k)
-
-    return benchmark_category, benchmarks_types
+    dir_path = "games"
+    for entry in os.scandir(dir_path):
+        if not entry.name.startswith('.') and entry.is_dir():
+            benchmark_category[entry.name] = []
+            # print(entry.name, entry.path, 0)
+            # print(os.listdir(f'games/{entry.name}'), 1)
+            for sub in os.scandir(f'games/{entry.name}'):
+                if not sub.name.startswith("_") and sub.name.endswith('.py') and sub.is_file():
+                    benchmark_category[entry.name].append({sub.name: sub.path})
+                    # print(sub.name, sub.path, 1)
+    # print(benchmark_category)
+    return benchmark_category
 
 
 
 def main():
-    benchmark_category,benchmarks_types  = benchmarkRetrieval()
+    benchmark_category  = benchmarkRetrieval()
     benchmarks = []
-    for k in benchmarks_types:
-        for item in benchmark_category[k]:
-            benchmarks.append(item)
-    # print(benchmarkRetrieval())
-    # print(benchmark_category.keys())
-    # print(benchmark_category.values())
-    # for k in benchmark_category.keys():
-    #     print(f"These benchmarks: {benchmark_category[k]} are for the \x1B[3m{k}\x1B[0m benchmark category")
+    benchmark_types = []
+    # I need to synthesis a benchmark_types
+    # I need to synthesis a benchmark list
+    for k in benchmark_category.keys():
+        benchmark_types.append(k)
+        # print(benchmark_category[k])
+        for pair in benchmark_category[k]:
+            # print(list(pair.keys())[0], 10)
+            benchmarks.append(list(pair.keys())[0])
+    # print(benchmark_types)
+    # print(benchmarks)
+
     parser = argparse.ArgumentParser(prog='red-queen',
                                     description='Red-Queen is a benchmark suite for quantum compilers')
     # parser.add_argument('--verbose', '-v')
     parser.add_argument('--version', action='version', version='%(prog)s 1.0.0')
     parser.add_argument("-c", "--compiler",choices=["qiskit", "tket", "tweedledum"], required=True, help='enter a compliler here')
-    parser.add_argument("-t","--benchmarkType", nargs='*', choices=benchmarks_types, help='enter the type of benchmark here')
+    parser.add_argument("-t","--benchmarkType", nargs='*', choices=benchmark_types, help='enter the type of benchmark here')
     parser.add_argument("-b","--benchmark", nargs='*', choices=benchmarks, help='enter the specfic benchmark here')
 
     args = parser.parse_args()
 
-    print(args.benchmarkType)
-    print(benchmarks_types)
-    # print(benchmark_category[args.benchmarkType[0]])
-    # print(set(args.benchmarkType).issubset(benchmark_category))
-    # print(args.benchmark, benchmark_category[args.benchmarkType])
-    print(args.benchmark is None)
-    print(args.benchmark is not None)
 
     ### Has a benchmark type been specified?
     if args.benchmarkType is not None:
@@ -88,13 +75,10 @@ def main():
         else:
             parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
     else:
-        question = input(f"would you like to run all benchmarks y/n  ")
+        question = input(f"Would you like to run all {len(benchmarks)} available benchmarks (y/n) ")
         if question.lower() == "y":
             print(f"run all benchmarks")
-        # parser.error(f"Please input a valid benchmark type:\x1B[3m{benchmarks_types}\x1B[0m")
-    # print(args)
-    # print(args.benchmarkCategory)
-    # print(args.benchmark)
+
     # if args.benchmark == None:
     #     benchmark = input()
     #     print(benchmark)
@@ -105,4 +89,5 @@ def main():
 
 
 if __name__ == "__main__":
+    benchmarkRetrieval()
     main()
