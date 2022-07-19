a7b909650345da7e7829b9f1c71b36f49783ada6
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2022-07-19 14:35:46 -0400

    Implementing Pytest Execution

--- red-queen.py
+++ red-queen.py
@@ -2,42 +2,38 @@
 import argparse
 import os
 from os import walk
+import sys
 from contextlib import redirect_stderr
-import subprocess as sub
+import subprocess
 from zoneinfo import available_timezones
-
+from pathlib import Path
 
 def benchmarkRetrieval():
     benchmark_category = {}
+    benchmark_types = []
+    benchmarks = []
     dir_path = "games"
     for entry in os.scandir(dir_path):
         if not entry.name.startswith('.') and entry.is_dir():
             benchmark_category[entry.name] = []
-            # print(entry.name, entry.path, 0)
-            # print(os.listdir(f'games/{entry.name}'), 1)
+            subDict = {}
             for sub in os.scandir(f'games/{entry.name}'):
                 if not sub.name.startswith("_") and sub.name.endswith('.py') and sub.is_file():
-                    benchmark_category[entry.name].append({sub.name: sub.path})
-                    # print(sub.name, sub.path, 1)
-    # print(benchmark_category)
-    return benchmark_category
+                    subDict[sub.name] = sub.path
+            benchmark_category[entry.name] = subDict
+
+    benchmark_types = list(benchmark_category.keys())
+
+    for benchmark_pairs in benchmark_category.values():
+        for keys in benchmark_pairs.keys():
+            benchmarks.append(keys)
+
+    return benchmark_category, benchmark_types, benchmarks
 
 
 
 def main():
-    benchmark_category  = benchmarkRetrieval()
-    benchmarks = []
-    benchmark_types = []
-    # I need to synthesis a benchmark_types
-    # I need to synthesis a benchmark list
-    for k in benchmark_category.keys():
-        benchmark_types.append(k)
-        # print(benchmark_category[k])
-        for pair in benchmark_category[k]:
-            # print(list(pair.keys())[0], 10)
-            benchmarks.append(list(pair.keys())[0])
-    # print(benchmark_types)
-    # print(benchmarks)
+    benchmark_category, benchmark_types, benchmarks  = benchmarkRetrieval()
 
     parser = argparse.ArgumentParser(prog='red-queen',
                                     description='Red-Queen is a benchmark suite for quantum compilers')
@@ -49,43 +45,57 @@ def main():
 
     args = parser.parse_args()
 
-
-    ### Has a benchmark type been specified?
-    if args.benchmarkType is not None:
-        print("passed test 0")
-        ### Is the inputted benchmark type valid?
-        if set(args.benchmarkType).issubset(benchmark_category):
-            print("passed test 1")
-            ### Has a benchmark been specified?
-            if args.benchmark is not None:
-                print("passed test 2")
-                ### Is the inputted benchmark valid?
-                if set(args.benchmark).issubset(benchmarks):
-                    print("passed test 3")
-                    ### Is the inputted benchmark within the inputted benchmark type suite?
-                    if set(args.benchmark).issubset(set(benchmark_category[args.benchmarkType[0]])):
-                        print("passed test 4")
+    # ### This for loop ensures that we are able to run various benchmark types
+    i = 0
+    while i < len(args.benchmarkType):
+        ### Has a benchmark type been specified?
+        if args.benchmarkType is not None:
+            # print("passed test 0")
+            ### Is the inputted benchmark type valid?
+            if set(args.benchmarkType).issubset(benchmark_category):
+                # print("passed test 1")
+                ### Has a benchmark been specified?
+                if args.benchmark is not None:
+                    print("passed test 2")
+                    ### Are the inputted benchmark(s) valid?
+                    if set(args.benchmark).issubset(benchmarks):
+                        print("passed test 3")
+                        ### Is the inputted benchmark within the inputted benchmark type suite?
+                        if set(args.benchmark).issubset(set(benchmark_category[args.benchmarkType[i]])):
+                            print("passed test 4")
+                            print(f"run {args.benchmark}")
+                            codeRan = subprocess.run([f"python -m pytest -s {benchmark_category[args.benchmarkType[i]][args.benchmark[i]]} -m {args.compiler} --store"],shell=True)
+                            i +=1
+                        else:
+                            parser.error(f"For the {args.benchmarkType} benchmark type you can only these benchmarks:\x1B[3m{benchmark_category[args.benchmarkType]}\x1B[0m")
                     else:
-                        parser.error(f"For the {args.benchmarkType} benchmark type you can only these benchmarks:\x1B[3m{benchmark_category[args.benchmarkType]}\x1B[0m")
+                        parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
                 else:
-                    parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
+                    # print(benchmark_category[args.benchmarkType[i]])
+                    for benchmark, path in benchmark_category[args.benchmarkType[i]].items():
+                        print(f"\x1B[3m{benchmark}\x1B[0m")
+                        # for benchmark, path in benchmark_pair.items():
+                        #     print(path)
+                    print(f"run all \x1B[3m{args.benchmarkType[i]}\x1B[0m benchmarks")
+                    i +=1
+                    # parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
             else:
-                print(f"run all {args.benchmarkType} benchmarks")
-                # parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
+                parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
         else:
-            parser.error(f"Please input a valid benchmark:\x1B[3m{benchmarks}\x1B[0m")
-    else:
-        question = input(f"Would you like to run all {len(benchmarks)} available benchmarks (y/n) ")
-        if question.lower() == "y":
-            print(f"run all benchmarks")
+            question = input(f"Would you like to run all {len(benchmarks)} available benchmarks (y/n) ")
+            if question.lower() == "y":
+                for benchmark_list in benchmark_category.values():
+                    for benchmark_pair in benchmark_list:
+                        for benchmark, path in benchmark_pair.items():
+                            print(path)
+                print(f"run all benchmarks")
 
     # if args.benchmark == None:
     #     benchmark = input()
     #     print(benchmark)
     # else:
     #     print(args.benchmark)
-    #     # sub.run("ls")
-
+    #     # subprocess.run("ls")
 
 
 if __name__ == "__main__":
