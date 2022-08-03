52274a074aac8acd86e35fb7d7d104b26e51208f
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2022-08-03 16:03:55 -0400

    Windows Patch

--- red_queen/cli.py
+++ red_queen/cli.py
@@ -16,74 +16,77 @@ def benchmarkRetrieval():
     benchmark_types = []
     benchmarks = []
     dir_path = "red_queen/games/"
-    windows_ad1 = ""
-    windows_ad2 = ""
-    if platform.system() == "Windows":
-        windows_ad1 = "python3 -m "
-        windows_ad2 = "-s "
     for entry in os.scandir(dir_path):
         if entry.is_dir():
             benchmark_category[entry.name] = []
-            subDict = {}
+            sub_dict = {}
             for sub in os.scandir(f"{dir_path}{entry.name}"):
                 if not sub.name.startswith("_") and sub.name.endswith(".py") and sub.is_file():
-                    subDict[sub.name] = sub.path
-            benchmark_category[entry.name] = subDict
+                    sub_dict[sub.name] = sub.path
+            benchmark_category[entry.name] = sub_dict
 
     benchmark_types = list(benchmark_category.keys())
     for benchmark_pairs in benchmark_category.values():
         for keys in benchmark_pairs.keys():
             benchmarks.append(keys)
 
-    return benchmark_category, benchmark_types, benchmarks, windows_ad1, windows_ad2
+    return benchmark_category, benchmark_types, benchmarks
 
 
-def complierRetrieval():
-    complierList = []
+def complier_retrieval():
+    complier_list = []
     config = configparser.ConfigParser()
     config.read("pytest.ini")
     for complier in config["pytest"]["markers"].split("\n"):
         if complier != "":
-            complierList.append(complier)
-    # print(complierList)
+            complier_list.append(complier)
+    # print(complier_list)
     # This line tests to see if there is a complier specifed
-    return complierList
+    return complier_list
 
 
-def resultRetrieval():
+def result_retrieval():
     results = {}
     dir_path = "results"
     for entry in os.scandir(dir_path):
         if entry.is_file():
             filename = entry.name
-            resultCount = int(filename.split("_")[0])
-            results[resultCount] = {entry.name: entry.path}
+            result_count = int(filename.split("_")[0])
+            results[result_count] = {entry.name: entry.path}
     return results
 
 
-def runBenchmarks(pytestPaths: str, windows_ad1: str, mTag: str, compiler: str):
+def run_benchmarks(pytest_paths: str, m_tag: str, compiler: str):
     click.echo("benchmarks ran")
-    subprocess.run(
-        [f"{windows_ad1}pytest -n auto {windows_ad2}{pytestPaths} {mTag}{compiler} --store"],
-        shell=True,
-    )
+    if platform.system() == "Windows":
+        subprocess.run(
+            [f"python -m pytest -s {pytest_paths} {m_tag}{compiler} --store"],
+            shell=True,
+            check=True,
+        )
+    else:
+        subprocess.run(
+            [f"pytest {pytest_paths} {m_tag}{compiler} --store"],
+            shell=True,
+            check=True,
+        )
 
 
-def showResult():
-    resultsDict = resultRetrieval()
-    click.echo(resultsDict)
-    resultNum = max(resultsDict.keys())
-    click.echo(resultNum)
-    result_path = tuple(resultsDict[resultNum].values())[0]
+def show_result():
+    results_dict = result_retrieval()
+    click.echo(results_dict)
+    result_num = max(results_dict.keys())
+    click.echo(result_num)
+    result_path = tuple(results_dict[result_num].values())[0]
     command = f"python3 -m report.console_tables --storage {result_path}"
-    subprocess.run([command], shell=True)
+    subprocess.run([command], shell=True, check=True)
     click.echo(
-        f"If you want to view the results chart type:\npython -m report.console_tables --storage {result_path}"
+        f"To view the results chart type:\npython -m report.console_tables --storage {result_path}"
     )
 
 
-benchmark_category, benchmark_types, benchmarks, windows_ad1, windows_ad2 = benchmarkRetrieval()
-complierList = complierRetrieval()
+benchmark_category, benchmark_types, benchmarks = benchmarkRetrieval()
+complier_list = complier_retrieval()
 
 
 @click.command()
@@ -93,7 +96,7 @@ complierList = complierRetrieval()
     "--compiler",
     "compiler",
     multiple=True,
-    type=click.Choice(complierList),
+    type=click.Choice(complier_list),
     help="enter a compliler here",
 )
 @click.option(
@@ -113,12 +116,12 @@ complierList = complierRetrieval()
     help="enter the specfic benchmark(s) here",
 )
 def main(compiler, benchmarkType, benchmark):
-    benchmarkPaths = []
-    pytestPaths = ""
-    myDict = {}
-    mTag = ""
+    benchmark_paths = []
+    pytest_paths = ""
+    mydict = {}
+    m_tag = ""
     if len(compiler) > 0:
-        mTag = "-m "
+        m_tag = "-m "
         compiler = compiler[0]
     else:
         compiler = ""
@@ -142,30 +145,30 @@ def main(compiler, benchmarkType, benchmark):
                         if set(benchmark).issubset(set(benchmark_category[benchmarkType[i]])):
                             # click.echo("passed test 4")
                             for j in range(len(benchmark)):
-                                benchmarkPaths.append(
+                                benchmark_paths.append(
                                     benchmark_category[benchmarkType[0]][benchmark[j]]
                                 )
-                            pytestPaths = " ".join(tuple(benchmarkPaths))
-                            runBenchmarks(pytestPaths, windows_ad1, mTag, compiler)
-                            showResult()
+                            pytest_paths = " ".join(tuple(benchmark_paths))
+                            run_benchmarks(pytest_paths, m_tag, compiler)
+                            show_result()
                             i += 1
                 else:
-                    myDict = benchmark_category[benchmarkType[0]]
-                    for v in myDict.values():
-                        benchmarkPaths.append(v)
-                    pytestPaths = " ".join(tuple(benchmarkPaths))
-                    runBenchmarks(pytestPaths, windows_ad1, mTag, compiler)
-                    showResult()
+                    mydict = benchmark_category[benchmarkType[0]]
+                    for v in mydict.values():
+                        benchmark_paths.append(v)
+                    pytest_paths = " ".join(tuple(benchmark_paths))
+                    run_benchmarks(pytest_paths, m_tag, compiler)
+                    show_result()
                     i += 1
     else:
         question = input(f"Would you like to run all {len(benchmarks)} available benchmarks (y/n) ")
         if question.lower() == "y":
             for benchmark_list in benchmark_category.values():
                 for paths in benchmark_list.values():
-                    benchmarkPaths.append(paths)
-            pytestPaths = " ".join(tuple(benchmarkPaths))
-            runBenchmarks(pytestPaths, windows_ad1, mTag, compiler)
-            showResult()
+                    benchmark_paths.append(paths)
+            pytest_paths = " ".join(tuple(benchmark_paths))
+            run_benchmarks(pytest_paths, m_tag, compiler)
+            show_result()
 
 
 if __name__ == "__main__":
