7ddb6734d018b00bf9e1fa40eb1f550311de74e6
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2022-08-08 16:24:58 -0400

    Lint cuz why not.

--- red_queen/games/characterization/run_qv.py
+++ red_queen/games/characterization/run_qv.py
@@ -8,6 +8,7 @@ from qiskit.providers.aer import QasmSimulator
 
 # Function to generate quantum volume circuits
 
+
 def generate_qv_circuits(num_qubits, numtrials=100):
     circuits = []
     for i in range(numtrials):
@@ -16,17 +17,28 @@ def generate_qv_circuits(num_qubits, numtrials=100):
         circuits.append(circuit)
     return circuits
 
+
 # Simulation (will be removed)
+
 
 def measure_qv(qc_list, backend=None, shots=1000):
     if backend == None:
-        results = [execute(qc, backend=Aer.get_backend('qasm_simulator'), shots=shots).result().get_counts() for qc in qc_list]
+        results = [
+            execute(qc, backend=Aer.get_backend("qasm_simulator"), shots=shots)
+            .result()
+            .get_counts()
+            for qc in qc_list
+        ]
         return results
     else:
-        results = [execute(qc, backend=backend, shots=shots).result().get_counts() for qc in qc_list]
+        results = [
+            execute(qc, backend=backend, shots=shots).result().get_counts() for qc in qc_list
+        ]
         return results
 
+
 # Get heavy outputs (Will be removed/modified)
+
 
 def get_heavy_outputs(counts):
     """Extract heavy outputs from counts dict.
@@ -39,10 +51,12 @@ def get_heavy_outputs(counts):
     # sort the keys of `counts` by value of counts.get(key)
     sorted_counts = sorted(counts.keys(), key=counts.get)
     # discard results with probability < median
-    heavy_outputs = sorted_counts[len(sorted_counts)//2:]
+    heavy_outputs = sorted_counts[len(sorted_counts) // 2 :]
     return heavy_outputs
 
+
 # Count heavy outputs (Will be removed/modified)
+
 
 def count_heavies(counts, heavies):
     num_heavies = 0
@@ -51,7 +65,9 @@ def count_heavies(counts, heavies):
             num_heavies += counts[i][key]
     return num_heavies
 
+
 # Calculate Threshold (Will be removed/modified)
+
 
 def check_threshold(nheavies, ncircuits, nshots):
     """Evaluate adjusted threshold inequality for quantum volume.
@@ -65,10 +81,13 @@ def check_threshold(nheavies, ncircuits, nshots):
             otherwise False
     """
     from numpy import sqrt
-    numerator = nheavies - 2*sqrt(nheavies*(nshots-(nheavies/ncircuits)))
-    return bool(numerator/(ncircuits*nshots) > 2/3), numerator/(ncircuits*nshots)
+
+    numerator = nheavies - 2 * sqrt(nheavies * (nshots - (nheavies / ncircuits)))
+    return bool(numerator / (ncircuits * nshots) > 2 / 3), numerator / (ncircuits * nshots)
+
 
 # General function (will be modified/removed)
+
 
 def bench_qv(qubits, backend=None, trials=100, shots=1000):
     circuits = generate_qv_circuits(qubits, trials)
@@ -76,5 +95,10 @@ def bench_qv(qubits, backend=None, trials=100, shots=1000):
     heavy_counts = [get_heavy_outputs(qc) for qc in results]
     num_heavies = count_heavies(results, heavy_counts)
     passes, threshold = check_threshold(num_heavies, trials, shots)
-    
-    return { "quantum_volume" : 2**qubits, "heavy_percent" : (num_heavies*100)/(trials*shots), "threshold" : threshold, "passes" : passes }
+
+    return {
+        "quantum_volume": 2**qubits,
+        "heavy_percent": (num_heavies * 100) / (trials * shots),
+        "threshold": threshold,
+        "passes": passes,
+    }
