0831f9313ea089e5eeab74dd17ae0a9e6afed8c8
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2022-08-09 00:09:11 -0400

    Added all Qasm Files, Draft __ini__.py method. - Generated qasm files for all qv ranging from 4 to 2^10. - Work in progress

--- red_queen/games/characterization/run_qv.py
+++ red_queen/games/characterization/run_qv.py
@@ -1,12 +1,17 @@
+import os
 import numpy as np
+import pytest
+from red_queen.games.characterization import backends, run_qiskit_quntum_volume
 
 # Importing standard Qiskit libraries
 from qiskit import QuantumCircuit, transpile, Aer, IBMQ, execute
 from qiskit.circuit.library import QuantumVolume
-from qiskit.compiler import transpiler
-from qiskit.providers.aer import QasmSimulator
-
 # Function to generate quantum volume circuits
+
+QASM_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qasm/qv/")
+
+
+CIRCUITS = [[QuantumCircuit.from_qasm_file(os.path.join(QASM_DIR, f"{i}qubits/qv{index}.qasm")) for index in range(150)] for i in range(2,11)]
 
 
 def generate_qv_circuits(num_qubits, numtrials=100):
@@ -17,88 +22,16 @@ def generate_qv_circuits(num_qubits, numtrials=100):
         circuits.append(circuit)
     return circuits
 
-
-# Simulation (will be removed)
-
-
-def measure_qv(qc_list, backend=None, shots=1000):
-    if backend == None:
-        results = [
-            execute(qc, backend=Aer.get_backend("qasm_simulator"), shots=shots)
-            .result()
-            .get_counts()
-            for qc in qc_list
-        ]
-        return results
-    else:
-        results = [
-            execute(qc, backend=backend, shots=shots).result().get_counts() for qc in qc_list
-        ]
-        return results
+@pytest.mark.qiskit
+@pytest.mark.parametrize("backend", backends)
+def bench_qiskit_qv(benchmark, backend):
+    shots = 1000
+    benchmark.name = "Quantum Volume"
+    run_qiskit_quntum_volume(benchmark, CIRCUITS, backend, shots)
 
 
-# Get heavy outputs (Will be removed/modified)
-
-
-def get_heavy_outputs(counts):
-    """Extract heavy outputs from counts dict.
-    Args:
-        counts (dict): Output of `.get_counts()`
-    Returns:
-        list: All states with measurement probability greater
-              than the mean.
-    """
-    # sort the keys of `counts` by value of counts.get(key)
-    sorted_counts = sorted(counts.keys(), key=counts.get)
-    # discard results with probability < median
-    heavy_outputs = sorted_counts[len(sorted_counts) // 2 :]
-    return heavy_outputs
-
-
-# Count heavy outputs (Will be removed/modified)
-
-
-def count_heavies(counts, heavies):
-    num_heavies = 0
-    for i, count in enumerate(heavies):
-        for j, key in enumerate(count):
-            num_heavies += counts[i][key]
-    return num_heavies
-
-
-# Calculate Threshold (Will be removed/modified)
-
-
-def check_threshold(nheavies, ncircuits, nshots):
-    """Evaluate adjusted threshold inequality for quantum volume.
-    Args:
-        nheavies (int): Total number of heavy outputs measured from device
-        ncircuits (int): Number of different square circuits run on device
-        nshots (int): Number of shots per circuit
-    Returns:
-        Bool:
-            True if heavy output probability is > 2/3 with 97% certainty,
-            otherwise False
-    """
-    from numpy import sqrt
-
-    numerator = nheavies - 2 * sqrt(nheavies * (nshots - (nheavies / ncircuits)))
-    return bool(numerator / (ncircuits * nshots) > 2 / 3), numerator / (ncircuits * nshots)
-
-
-# General function (will be modified/removed)
-
-
-def bench_qv(qubits, backend=None, trials=100, shots=1000):
-    circuits = generate_qv_circuits(qubits, trials)
-    results = measure_qv(circuits, backend, shots)
-    heavy_counts = [get_heavy_outputs(qc) for qc in results]
-    num_heavies = count_heavies(results, heavy_counts)
-    passes, threshold = check_threshold(num_heavies, trials, shots)
-
-    return {
-        "quantum_volume": 2**qubits,
-        "heavy_percent": (num_heavies * 100) / (trials * shots),
-        "threshold": threshold,
-        "passes": passes,
-    }
+if __name__ == "__main__":
+    for i in range(2,11):
+        circuits = generate_qv_circuits(i,150)
+        for index, circuit in enumerate(circuits):
+            circuit.qasm(filename=os.path.join(QASM_DIR, f"{i}qubits/qv{index}.qasm"))
